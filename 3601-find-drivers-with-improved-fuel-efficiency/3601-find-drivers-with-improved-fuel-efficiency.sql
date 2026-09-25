6e42dc3ab866a74ad3efcdc01ff85b5a86c874ce
senpai_hub <babydwarf22@gmail.com>
2026-09-26 00:52:44 +0530

    Time: 575 ms (66.08%), Space: 0B (100%) - LeetHub

---
+++ 3601-find-drivers-with-improved-fuel-efficiency/3601-find-drivers-with-improved-fuel-efficiency.sql
@@ -0,0 +1,30 @@
+# Write your MySQL query statement below
+WITH tab_a AS ( 
+    SELECT
+        *,
+        distance_km/fuel_consumed AS fuel_efficiency,
+        CASE 
+            WHEN MONTH(trip_date) IN (1, 2, 3, 4, 5, 6) THEN "first_half"
+            WHEN MONTH(trip_date) IN (7, 8, 9, 10, 11, 12) THEN "second_half"
+        END AS term
+    FROM trips
+)
+SELECT
+    t.driver_id,
+    d.driver_name,
+    ROUND( AVG(CASE 
+            WHEN t.term = "first_half" THEN t.fuel_efficiency
+            END), 2) AS first_half_avg,
+    ROUND( AVG(CASE 
+            WHEN t.term = "second_half" THEN t.fuel_efficiency
+            END), 2) AS second_half_avg,
+    ROUND( AVG(CASE 
+            WHEN t.term = "second_half" THEN t.fuel_efficiency
+            END) -  AVG(CASE 
+            WHEN t.term = "first_half" THEN t.fuel_efficiency
+            END), 2) AS efficiency_improvement      
+FROM tab_a t JOIN drivers d
+    ON t.driver_id = d.driver_id
+GROUP BY t.driver_id
+HAVING second_half_avg > first_half_avg
+ORDER BY 5 DESC, 2 ASC;
